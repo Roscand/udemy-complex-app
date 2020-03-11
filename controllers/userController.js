076c1b299ce869e6706541edfd6954bd68c14536
RoscandDesign <roscanddesign@gmail.com>
2020-03-11 18:33:45 +0400

    Registration Validation

--- controllers/userController.js
+++ controllers/userController.js
@@ -9,8 +9,9 @@ exports.logout = function(req, res) {
 };
 
 exports.register = function(req, res) {
-    let user = new User();
+    let user = new User(req.body);
     user.register();
+    res.send('User Successfully Registered!');
 };
 
 exports.home = function(req, res) {
--- modules/User.js
+++ modules/User.js
@@ -1,4 +1,5 @@
-
+const validator = require('validator');
+const userCollection = require('../db').db().collection('users');
 
 const User = function(data) {
     this.data = data;
@@ -6,15 +7,34 @@ const User = function(data) {
 };
 
 User.prototype.cleanUp = function() {
+    if (typeof(this.data.username) !== "string") {this.data.username = ""};
+    if (typeof(this.data.email) !== "string") {this.data.email = ""};
+    if (typeof(this.data.password) !== "string") {this.data.password = ""};
 
+    this.data = {
+        username: this.data.username.trim().toLowerCase(),
+        email: this.data.email.trim().toLowerCase(),
+        password: this.data.password
+    };
 };
 
 User.prototype.validate = function() {
-
+    if (this.data.username == "") {this.errors.push("You must provide a username.")};
+    if (this.data.username != "" && !validator.isAlphanumeric(this.data.username)) {this.errors.push("Username can only contain numbers and letters.")};
+    if (this.data.username.length != "" && this.data.username.length < 3) {this.errors.push("Username must be at least 3 characters long.")};
+    if (this.data.username.length > 30) {this.errors.push("Username length cannot exceed 30 characters.")};
+    if (!validator.isEmail(this.data.email)) {this.errors.push("You must provide a valid email address.")};
+    if (this.data.password == "") {this.errors.push("You must provide a password.")};
+    if (this.data.password.length != "" && this.data.password.length < 12) {this.errors.push("Password must be at least 12 characters long.")};
+    if (this.data.password.length > 50) {this.errors.push("Password length cannot exceed 50 characters.")};
 };
 
 User.prototype.register = function() {
-
+    this.cleanUp();
+    this.validate();
+    if (!this.errors.length) {
+        userCollection.insertOne(this.data);
+    };
 };
 
 module.exports = User;
